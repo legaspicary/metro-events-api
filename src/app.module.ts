5cb8952e5dae9e24d5d59b181a50fee1494cb42f
Cary Legaspi <legaspicary@gmail.com>
2021-11-16 21:29:59 +0800

    Setup database

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,29 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { EventModule } from './event/event.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { readFileSync } from 'fs';
 @Module({
-  imports: [UserModule, EventModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: 5432,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: 'metroevents',
+      entities: [],
+      synchronize: true,
+      ssl: {
+        rejectUnauthorized: false,
+        ca: [readFileSync('DigiCertGlobalRootCA.crt.pem', 'utf8')],
+      },
+    }),
+    UserModule,
+    EventModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
